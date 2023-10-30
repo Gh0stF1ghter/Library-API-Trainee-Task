67f128e3e14da8474b43585850026878e74c13b4
Dima <dmitriygorchanin@gmail.com>
2023-10-31 00:42:59 +0300

    refactor: delete uneccessary assignments

--- Data/UnitOfWork.cs
+++ Data/UnitOfWork.cs
@@ -1,32 +1,33 @@
 ﻿using Core;
 using Core.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly LibraryContext _context;
-        private readonly Repository<Author>? _authorRepository;
-        private readonly Repository<Genre>? _genreRepository;
-        private readonly Repository<Book>? _bookRepository;
 
         public UnitOfWork(LibraryContext context) => _context = context;
 
-        public IRepository<Author> Authors => _authorRepository ?? new Repository<Author>(_context);
-        public IRepository<Genre> Genres => _genreRepository ?? new Repository<Genre>(_context);
-        public IRepository<Book> Books => _bookRepository ?? new Repository<Book>(_context);
+        public IRepository<Author> Authors => new Repository<Author>(_context);
+        public IRepository<Genre> Genres => new Repository<Genre>(_context);
+        public IRepository<Book> Books => new Repository<Book>(_context);
 
         public async Task<int> CommitAsync() => await _context.SaveChangesAsync();
 
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed && disposing)
+            {
+                _context.Dispose();
+            }
+            disposed = true;
+        }
+
         public void Dispose()
         {
-            _context.Dispose();
+            Dispose(true);
             GC.SuppressFinalize(this);
         }
     }
