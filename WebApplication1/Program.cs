dce0aa143976db797bcf95bf6e9c433f80dfb6bb
Dima <dmitriygorchanin@gmail.com>
2023-10-31 00:43:50 +0300

    feat: configure database creation & migration

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,7 @@
 using Core;
 using Core.Services;
 using Data;
+using Microsoft.EntityFrameworkCore;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 var services = builder.Services;
-
 services.AddScoped<IUnitOfWork, UnitOfWork>();
 services.AddTransient<IGenreService, GenreService>();
 services.AddTransient<IAuthorService, AuthorService>();
 services.AddTransient<IBookService, BookService>();
+services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Data")));
 services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
@@ -23,14 +24,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    _ = app.UseSwagger();
-    _ = app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 
 app.MapControllers();
 
