69ebdb05fc34b97059f2706829f3d64e0d090d9d
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-10-30 23:10:27 +0300

    feat: add LastName property

--- Core/Models/Author.cs
+++ Core/Models/Author.cs
@@ -10,7 +10,8 @@ namespace Core.Models
     public class Author
     {
         public int AuthorId { get; set; }
-        public string Name { get; set; } = null!;
+        public string AuthorFirstMidName { get; set; } = null!;
+        public string AuthorLastName { get; set; } = null!;
 
         public ICollection<Book> Books { get; set; } = new Collection<Book>();
     }
