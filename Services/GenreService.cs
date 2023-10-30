274b4a5c137e63ff22a84717f6e6032c4a483cf5
Dima <dmitriygorchanin@gmail.com>
2023-10-31 00:47:27 +0300

    feat: implement Author & Genre services

--- Services/GenreService.cs
+++ Services/GenreService.cs
@@ -1,4 +1,6 @@
-﻿using Core.Services;
+﻿using Core;
+using Core.Models;
+using Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,31 @@ namespace Services
 {
     public class GenreService : IGenreService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public GenreService(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        
+        public async Task<IEnumerable<Genre>> GetAllGenresAsync() => await _unitOfWork.Genres.GetAllAsync();
+        public async ValueTask<Genre?> GetGenreByIdAsync(int id) => await _unitOfWork.Genres.GetByIdAsync(id);
+
+        public async Task<Genre> CreateGenreAsync(Genre genre)
+        {
+            await _unitOfWork.Genres.AddAsync(genre);
+            await _unitOfWork.CommitAsync();
+            return genre;
+        }
+
+
+        public async Task UpdateGenreAsync(Genre oldGenre, Genre newGenre)
+        {
+            oldGenre.GenreName = newGenre.GenreName;
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteGenreAsync(Genre genre)
+        {
+            _unitOfWork.Genres.Remove(genre);
+            await _unitOfWork.CommitAsync();
+        }
+
     }
 }
