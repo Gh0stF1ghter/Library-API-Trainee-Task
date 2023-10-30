37659b8ac9487d46573dd1139261e8359fda35d8
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-10-30 23:12:04 +0300

    feat: implement Database context

--- Data/LibraryContext.cs
+++ Data/LibraryContext.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Models;
+using Data.Configurations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
     public class LibraryContext : DbContext
     {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new GenreConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new BookGenreConfiguration());
+        }
     }
 }
