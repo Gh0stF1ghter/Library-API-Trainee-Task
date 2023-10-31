217f07db2dcfc7f45437bb2dc8bc9e06e549a42d
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-10-31 18:43:11 +0300

    feat: add auth via JWT bearer

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,7 +1,10 @@
 using Core;
 using Core.Services;
 using Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,15 +12,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 var services = builder.Services;
+
+services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Data")));
+
+services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<LibraryContext>()
+    .AddDefaultTokenProviders();
+
+services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+});
+
 services.AddScoped<IUnitOfWork, UnitOfWork>();
 services.AddTransient<IGenreService, GenreService>();
 services.AddTransient<IAuthorService, AuthorService>();
 services.AddTransient<IBookService, BookService>();
-services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Data")));
+
+
+services.AddAutoMapper(typeof(Program));
+
 services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
+services.AddSwaggerGen(s => s.SwaggerDoc("v1", new OpenApiInfo { Title = "Libary", Version = "v1" }));
 
 var app = builder.Build();
 
@@ -28,10 +54,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
