df17bacc16c96b952cbd76e3da36d7189454b3a6
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-10-31 18:42:15 +0300

    feat: implement mapper

--- WebApplication1/Mapping/MappingProfile.cs
+++ WebApplication1/Mapping/MappingProfile.cs
@@ -1,8 +1,24 @@
-﻿using AutoMapper;
+﻿using API.Resources;
+using AutoMapper;
+using Core.Models;
 
 namespace API.Mappers
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Author, AuthorResource>();
+            CreateMap<Genre, GenreResource>();
+            CreateMap<Book, BookResource>();
+
+            CreateMap<AuthorResource, Author>();
+            CreateMap<GenreResource, Genre>();
+            CreateMap<BookResource, Book>();
+
+            CreateMap<SaveAuthorResource, Author>();
+            CreateMap<SaveGenreResource, Genre>();
+            CreateMap<SaveBookResource, Book>();
+        }
     }
 }
