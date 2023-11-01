d4718d1e73593c89ec5867ced3a75a5bf3d4f322
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-11-01 23:18:37 +0300

    feat: implement models for authentication

---
+++ Core/Models/Auth/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Auth
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Invalid input")]
+        public string Username { get; set; } = null!;
+
+        [PasswordPropertyText]
+        [Required(ErrorMessage = "Invalid input")]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Core/Models/Auth/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Auth
+{
+    public class User : IdentityUser
+    {
+        public string FirstMidName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
