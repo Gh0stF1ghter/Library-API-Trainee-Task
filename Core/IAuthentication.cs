fc838d0b0d275a89b7e6932aa3644af90ab9cc6b
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-11-01 23:18:56 +0300

    feat: implement IAuthentication interface

---
+++ Core/IAuthentication.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IAuthentication
+    {
+        Task<(bool, string)> RegisterAsync(Register register, string role);
+        Task<(bool, string)> AuthenticateAsync(Login login);
+
+    }
+}
