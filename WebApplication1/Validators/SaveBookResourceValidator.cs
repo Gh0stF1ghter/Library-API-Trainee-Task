2bae0d4ad31efba722a68c888e6db242df01e854
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2023-11-02 22:51:15 +0300

    feat: add ISBN validator

--- WebApplication1/Validators/SaveBookResourceValidator.cs
+++ WebApplication1/Validators/SaveBookResourceValidator.cs
@@ -1,5 +1,5 @@
-﻿using FluentValidation;
-using API.Resources;
+﻿using API.Resources;
+using FluentValidation;
 
 namespace API.Validators
 {
@@ -16,12 +16,27 @@ namespace API.Validators
                 .NotEmpty()
                 .MinimumLength(1)
                 .MaximumLength(50)
-                .WithMessage("Book Name cannot be null");
+                .WithMessage("Book Name cannot be null or longer than 50 characters");
 
             RuleFor(b => b.BookISBN)
-                .NotEmpty()
-                .NotNull()
-                .Length(13);
+                .Length(13)
+                .WithMessage("ISBN should have 13 digits")
+                .Must(x =>
+                {
+                    var chars = x.ToCharArray();
+                    double result = 0;
+
+                    for (int i = 1; i < 13; i += 2)
+                        result += char.GetNumericValue(chars[i]) * 3;
+                    for (int i = 0; i < 13; i += 2)
+                        result += char.GetNumericValue(chars[i]);
+
+                    return result % 10 == 0;
+                })
+                .WithMessage("Isbn check digit is invalid. See ... for details");
+
+            RuleFor(b => b.BookReturnDate)
+                .GreaterThan(b => b.BookTakeDate);
         }
     }
 }
